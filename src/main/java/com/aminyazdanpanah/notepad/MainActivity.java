c49266d5ff68fe7a8db5cc7c39fd2252ae24d873
Amin Yazdanpanah <amin69y@gmail.com>
2016-12-02 23:50:57 +0330

    Update MainActivity.java

--- src/main/java/com/aminyazdanpanah/notepad/MainActivity.java
+++ src/main/java/com/aminyazdanpanah/notepad/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +23,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 /**
@@ -29,9 +31,10 @@ import java.util.Arrays;
  */
 public class MainActivity extends AppCompatActivity {
 
-    String [] filename,filepath;
+    String[] filename, filepath, fileId, FilePN;
     DatabaseHelper helper = new DatabaseHelper(this);
     ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,27 +46,38 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent createText = new Intent(MainActivity.this,CreateText.class);
+                Intent createText = new Intent(MainActivity.this, CreateText.class);
                 startActivity(createText);
             }
         });
-        TextView text = (TextView)findViewById(R.id.text);
+        getList();
+    }
+    public void getList() {
+        TextView text = (TextView) findViewById(R.id.text);
         Cursor data = helper.getListContents();
         int count = 0;
         filename = new String[data.getCount()];
         filepath = new String[data.getCount()];
-        while(data.moveToNext()) {
+        fileId = new String[data.getCount()];
+        while (data.moveToNext()) {
+            fileId[count] = data.getString(0);
             filename[count] = data.getString(1);
             filepath[count] = data.getString(2);
             count++;
         }
-        if(data.getCount() == 0){
-            text.setText("There are no notes in this list! Please add one +!");
-        }else{
-            lv=(ListView) findViewById(R.id.listView);
-            lv.setAdapter(new TextList(this, filename,filepath));
+        lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(null);
+        if (data.getCount() == 0) {
+            text.setText("There are no notes in this list! Please create one +!");
+        } else {
+            lv.setAdapter(new TextList(this, filename, filepath, fileId));
         }
-
+    }
+    public void onDeleteButtonClick(View v) {
+        String File = v.getTag().toString();
+        FilePN = File.split("--");
+        AlertDialog diaBox = delete();
+        diaBox.show();
     }
 
     @Override
@@ -83,12 +97,13 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
     private AlertDialog about() {
 
         AlertDialog myQuittingDialogBox = new AlertDialog.Builder(this)
 
                 .setTitle("About")
-                .setMessage("The app was created by Amin Yazdanpanah \n Please share your idea about this app to my \n http://www.LinkedIn.com/aminyazdanpanah")
+                .setMessage("The app was created by Amin Yazdanpanah \n Please share your idea about this app to my website \n http://www.aminyazdanpanah.com")
                 .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
@@ -97,5 +112,78 @@ public class MainActivity extends AppCompatActivity {
                 .create();
         return myQuittingDialogBox;
 
+    }
+
+    private AlertDialog delete() {
+
+        AlertDialog deleteMessage = new AlertDialog.Builder(this)
+
+                .setTitle("Delete File")
+                .setMessage("Are you sure you want to permanently delete " + FilePN[1] + "?")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteFile(FilePN[0], FilePN[1], FilePN[2]);
+                    }
+                })
+
+                .create();
+        return deleteMessage;
+
+    }
+
+    public void deleteFile(String FileID, String FileName, String FilePath) {
+        if (!FilePath.matches("")) {
+            FilePath = FilePath + "/";
+        }
+        String fpath = Environment.getExternalStoragePublicDirectory("").toString() + "/" + FilePath + FileName + ".txt";
+        File file = new File(fpath);
+        boolean deleted = file.delete();
+        if (deleted) {
+            Toast.makeText(getBaseContext(), FileName + " was deleted!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getBaseContext(), "Already your file was deleted!", Toast.LENGTH_LONG).show();
+        }
+        helper.deleteText(Integer.parseInt(FileID));
+        getList();
+    }
+
+
+
+    public void onBackPressed() {
+        AlertDialog diaBox = exit();
+        diaBox.show();
+    }
+
+    private AlertDialog exit()
+    {
+        AlertDialog exitM =new AlertDialog.Builder(this)
+                .setTitle("Exit")
+                .setMessage("Do you want to exit NotePad?")
+
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        return exitM;
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
     }
 }
